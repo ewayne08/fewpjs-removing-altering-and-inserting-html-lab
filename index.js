6bef02b1cf65e633d3f543c980fb750f2a2f8954
Ethan <ethanchristianwayne@gmail.com>
2021-06-01 23:34:58 -0400

    commit tree b2e7c4691eab80448cb8cfb05249ec897ec62f53

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let body = document.querySelector("body");
+body.removeChild(body.querySelector('#main'));
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "YOUR-NAME is the champion";
